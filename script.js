7222f9d6ab5debfd540064d85d359194351b836c
usha1126 <upparausharani2002@gmail.com>
2026-02-01 17:31:49 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,6 @@
+function submmited() {
+    document.getElementById("submit").addEventListener("click" , function clicked() {
+        alert("Submitted sccessfully..!");
+    });
+    return clicked();
+}
